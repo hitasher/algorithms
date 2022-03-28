4506b42db3b880ffe7ce93a341bca50f8c81c528
hitasher <hitasher@gmail.com>
2022-03-28 21:05:47 +0400

    Implement `SelectionSort` via `AbstractSortingAlgorithm`

--- selection_sort.py
+++
@@ -1,9 +0,0 @@
-from typing import MutableSequence
-
-from local_typing import Comparable
-
-
-def selection_sort(sequence: MutableSequence[Comparable]) -> None:
-    for i in range(len(sequence)):
-        index_of_minimum_element: int = min(range(i, len(sequence)), key=sequence.__getitem__)
-        sequence[i], sequence[index_of_minimum_element] = sequence[index_of_minimum_element], sequence[i]
